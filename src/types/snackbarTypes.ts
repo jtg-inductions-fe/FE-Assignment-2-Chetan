e9d2229e0f121cc3a9f40623984dad0d9552820c
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-19 20:04:01 +0530

    [CD_A3_01]: Implement signup page, login page, error handling , not found page, 500 error page and error boundary

---
+++ src/types/snackbarTypes.ts
@@ -0,0 +1,8 @@
+import { AlertColor } from '@mui/material';
+
+export interface SnackbarState {
+    open: boolean;
+    message: string;
+    severity: AlertColor;
+    autoHideDuration: number;
+}
