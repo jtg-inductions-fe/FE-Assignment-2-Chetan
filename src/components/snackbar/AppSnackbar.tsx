aabdb747b43279f98e11094ea83cad25c159ad57
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-19 20:04:01 +0530

    [CD_A3_01]: Implement signup page, login page, error handling , not found page, 500 error page and error boundary

---
+++ src/components/snackbar/AppSnackbar.tsx
@@ -0,0 +1,32 @@
+import { Alert, Snackbar } from '@mui/material';
+
+import { hideSnackbar } from '@features/snackbar/snackbarSlice';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
+
+const AppSnackbar = () => {
+    const dispatch = useAppDispatch();
+
+    const { open, message, severity, autoHideDuration } = useAppSelector((state) => state.snackbar);
+
+    const handleClose = () => {
+        dispatch(hideSnackbar());
+    };
+
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={autoHideDuration}
+            onClose={handleClose}
+            anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+            }}
+        >
+            <Alert onClose={handleClose} severity={severity} variant="filled">
+                {message}
+            </Alert>
+        </Snackbar>
+    );
+};
+
+export default AppSnackbar;
