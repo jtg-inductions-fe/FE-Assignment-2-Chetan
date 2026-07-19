7ed3eb79cc58e98d14ba15ea4214bdf466ccebd2
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-19 20:04:01 +0530

    [CD_A3_01]: Implement signup page, login page, error handling , not found page, 500 error page and error boundary

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,3 +1,6 @@
+import { authApi } from '@api/authApi';
+import authReducer from '@features/auth/authSlice';
+import snackbarReducer from '@features/snackbar/snackbarSlice';
 import { combineReducers } from '@reduxjs/toolkit';
 import { authApi, menuItemsApi, ordersApi, restaurantsApi } from '@services';
 import { authReducer, cartReducer, snackbarReducer } from '@slices';
