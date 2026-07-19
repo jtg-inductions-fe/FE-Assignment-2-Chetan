992f29cf5c905f4570d3db884f55cee0deb5b963
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-20 03:52:13 +0530

    [CD_A3_02]: Implement header and sidebar

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import NotFound from '@components/errorHandlingPages/NotFound';
+import MainLayout from '@components/layouts/MainLayout';
 import { ROUTES } from '@constants';
 import Login from '@features/auth/pages/Login';
 import Signup from '@features/auth/pages/Signup';
@@ -9,11 +10,14 @@ import GuestRoute from '@routes/GuestRoute';
 
 const AppRoutes = () => (
     <Routes>
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route element={<GuestRoute />}>
-            <Route path={ROUTES.AUTH.SIGNUP} element={<Signup />} />
-            <Route path={ROUTES.AUTH.LOGIN} element={<Login />} />
+        <Route element={<MainLayout />}>
+            <Route element={<GuestRoute />}>
+                <Route path={ROUTES.AUTH.SIGNUP} element={<Signup />} />
+                <Route path={ROUTES.AUTH.LOGIN} element={<Login />} />
+            </Route>
+            <Route path="/" element={<Home />} />
         </Route>
+
         <Route path="*" element={<NotFound />} />
     </Routes>
 );
