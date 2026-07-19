a25853b7104547e3ebdce28cb01fa23cb8d5057a
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-19 20:04:01 +0530

    [CD_A3_01]: Implement signup page, login page, error handling , not found page, 500 error page and error boundary

---
+++ src/features/auth/authStyle.ts
@@ -0,0 +1,30 @@
+import { Box, Paper } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export const AuthContainer = styled(Box)(({ theme }) => ({
+    minHeight: '100vh',
+    ...theme.mixins.flexLayout(),
+    backgroundColor: theme.palette.background.default,
+}));
+
+export const AuthCard = styled(Paper)(({ theme }) => ({
+    width: '100%',
+    maxWidth: 400,
+    paddingInline: theme.spacing(4),
+    paddingBlock: theme.spacing(10),
+    backgroundColor: theme.palette.background.default,
+}));
+
+export const SignupContainer = styled(Box)(({ theme }) => ({
+    minHeight: '100vh',
+    ...theme.mixins.flexLayout(),
+    backgroundColor: theme.palette.background.default,
+}));
+
+export const SignCard = styled(Paper)(({ theme }) => ({
+    width: '100%',
+    maxWidth: 600,
+    padding: theme.spacing(4),
+
+    backgroundColor: theme.palette.background.default,
+}));
