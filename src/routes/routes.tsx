ca672b577fb13b863732cf9be964cd146499c006
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-21 00:46:31 +0530

    [CD_A3_01]: fixes PR reviews

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,7 +4,7 @@ import { ROUTES } from '@constants';
 import { Home } from '@pages';
 import { Login, NotFound, Signup } from '@pages';
 
-import GuestRoute from './Guest.route';
+import { GuestRoute } from './guest.route';
 
 export const AppRoutes = () => (
     <Routes>
