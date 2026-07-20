eef2173db0f99eb2458c55736d59d7f7fdafa7e2
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-21 03:00:48 +0530

    [CD_A3_01]: Implement pxTorem function

--- src/containers/login/Login.style.ts
+++ src/containers/login/Login.style.ts
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 
 export const LoginCard = styled(Paper)(({ theme }) => ({
     width: '100%',
-    maxWidth: 400,
+    maxWidth: theme.typography.pxToRem(400),
     paddingInline: theme.spacing(4),
     paddingBlock: theme.spacing(10),
     backgroundColor: theme.palette.background.default,
