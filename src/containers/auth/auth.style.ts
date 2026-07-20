c6a651e6b578d3e7c62147b0e337ea6609a75a75
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-21 00:46:31 +0530

    [CD_A3_01]: fixes PR reviews

--- src/containers/auth/auth.style.ts
+++
@@ -1,8 +0,0 @@
-import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-export const AuthContainer = styled(Box)(({ theme }) => ({
-    minHeight: '100vh',
-    ...theme.mixins.flexLayout(),
-    backgroundColor: theme.palette.background.default,
-}));
