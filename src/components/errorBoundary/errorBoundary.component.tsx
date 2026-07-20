99fedcaa9ba50b074bbf8ed303f1c52ed9a65112
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-20 23:19:36 +0530

    [CD_A3_01]: fixes the PR reviews.

--- src/components/errorBoundary/errorBoundary.component.tsx
+++ src/components/errorBoundary/errorBoundary.component.tsx
@@ -1,8 +1,11 @@
 import { Component } from 'react';
 
-import { Box, Button, Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 
-import { ErrorBoundaryProps, ErrorBoundaryState } from './error-boundary.types';
+import error500 from '@assets/image/error500.webp';
+import { ErrorImage } from '@styles';
+
+import { ErrorBoundaryProps, ErrorBoundaryState } from './errorBoundary.types';
 
 export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     state: ErrorBoundaryState = {
@@ -19,15 +22,7 @@ export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundarySt
         if (this.state.hasError) {
             return (
                 <Stack justifyContent="center" alignItems="center" sx={{ minHeight: '80vh', p: 2 }}>
-                    <Box
-                        component="img"
-                        src="/image/error500.png"
-                        alt="Something went wrong "
-                        sx={{
-                            width: '100%',
-                            maxWidth: 940,
-                        }}
-                    />
+                    <ErrorImage src={error500} alt="Something went wrong" />
 
                     <Button variant="contained" onClick={() => this.setState({ hasError: false })}>
                         Try Again
