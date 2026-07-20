2cdbeffc236f7c012da89aee96b95dee48d2e545
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-20 16:08:03 +0530

    [CD_A3_00]: update folder structre and path aliasing

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-export { COLORS, HTML_FONT_SIZE, SCALING_FACTOR, FONT_WEIGHT } from './themeConstants';
-export { ERROR_CODES } from './errorConstants';
-export { HTTP_METHODS } from './httpConstants';
-export { API_ROUTES } from './apiRouteConstants';
-export { ROUTES } from './routeConstants';
-export { BREAKPOINTS } from './breakpointsConstants';
+export { COLORS, HTML_FONT_SIZE, SCALING_FACTOR, FONT_WEIGHT } from './theme.constants';
+export { ERROR_CODES } from './error.constants';
+export { HTTP_METHODS } from './http-methods.constants';
+export { API_ROUTES } from './api-routes.constants';
+export { ROUTES } from './routes.constants';
+export { BREAKPOINTS } from './breakpoints.constants';
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import { AppRoutes } from '@routes/appRoutes';
+import { AppRoutes } from '@routes/routes';
 import store from '@store/store';
 import { theme } from '@theme';
 
