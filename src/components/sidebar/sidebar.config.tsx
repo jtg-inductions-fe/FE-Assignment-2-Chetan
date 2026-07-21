175b38c95f9d0a0776e45ec81319d0c5722fb9ae
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-22 00:31:37 +0530

    [CD_A3_02]: apply styling on header and sidebar

--- src/components/sidebar/sidebar.config.tsx
+++ src/components/sidebar/sidebar.config.tsx
@@ -1,13 +1,22 @@
-import { ROUTES } from './routes.constants';
+import { History, Home, Person2, QueryStats, Restaurant } from '@mui/icons-material';
+
+import { ROUTES } from '@constants';
 
 export const SIDEBAR_ADMIN_LIST = [
     {
         label: 'Home',
         path: ROUTES.HOME,
+        icon: <Home />,
     },
     {
         label: 'Restaurants',
         path: ROUTES.DASHBOARD.RESTAURANTS.ROOT,
+        icon: <Restaurant />,
+    },
+    {
+        label: 'Analytics',
+        path: ROUTES.DASHBOARD.RESTAURANTS.ROOT,
+        icon: <QueryStats />,
     },
 ] as const;
 
@@ -15,13 +24,16 @@ export const SIDEBAR_USER_LIST = [
     {
         label: 'Home',
         path: ROUTES.HOME,
+        icon: <Home />,
     },
     {
         label: 'Profile',
         path: ROUTES.DASHBOARD.UPDATE_PROFILE,
+        icon: <Person2 />,
     },
     {
         label: 'Past Orders',
         path: ROUTES.DASHBOARD.PAST_ORDERS,
+        icon: <History />,
     },
 ] as const;
