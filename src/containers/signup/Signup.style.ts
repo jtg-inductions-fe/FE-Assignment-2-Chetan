551ef64f1019a320475d20c2c262ea08861b14c0
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-21 12:55:12 +0530

    [CD_A3_01] : fixes reviews

--- src/containers/signup/Signup.style.ts
+++
@@ -1,9 +0,0 @@
-import { Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-export const SignupCard = styled(Paper)(({ theme }) => ({
-    width: '100%',
-    maxWidth: theme.typography.pxToRem(600),
-    padding: theme.spacing(4),
-    backgroundColor: theme.palette.background.default,
-}));
