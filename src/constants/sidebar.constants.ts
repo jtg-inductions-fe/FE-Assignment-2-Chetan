3107fc5a2664096561f92565dfb5749681a4c1ab
Ubuntu <chetan.dhaked@joshtechnologygroup.com>
2026-07-21 18:24:51 +0530

    [CD_A3_02]: Implement accessbility in header and sidebar

--- src/constants/sidebar.constants.ts
+++ src/constants/sidebar.constants.ts
@@ -1,1 +1,27 @@
-export const SIDEBAR_LIST = ['Profile', 'Balance', 'Past Orders', 'Restaurants', 'stats'];
+import { ROUTES } from './routes.constants';
+
+export const SIDEBAR_ADMIN_LIST = [
+    {
+        label: 'Home',
+        path: ROUTES.HOME,
+    },
+    {
+        label: 'Restaurants',
+        path: ROUTES.DASHBOARD.RESTAURANTS.ROOT,
+    },
+] as const;
+
+export const SIDEBAR_USER_LIST = [
+    {
+        label: 'Home',
+        path: ROUTES.HOME,
+    },
+    {
+        label: 'Profile',
+        path: ROUTES.DASHBOARD.UPDATE_PROFILE,
+    },
+    {
+        label: 'Past Orders',
+        path: ROUTES.DASHBOARD.PAST_ORDERS,
+    },
+] as const;
